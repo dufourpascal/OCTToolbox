27942cc16bce45904cd5255dbec910963f4f5a53
Pascal Andre Dufour <dufourp83@gmail.com>
2014-08-18 16:42:06 +0200

    updated mean computation

--- filtering/meanAboveBelow.m
+++ filtering/meanAboveBelow.m
@@ -4,16 +4,15 @@ function [ meanImgAbove, meanImgBelow ] = meanAboveBelow( img2d )
 [ny,nx] = size(img2d);
 type = class(img2d);
 
-meanImgAbove = zeros(ny,nx, type);
-meanImgBelow = zeros(ny,nx, type);
-
 cImg = cumsum(img2d,1); %cumulative image
+cImgFlipped = cumsum(flipud(img2d),1);
 
-for y = 1:ny-1
-  for x = 1:nx
-    meanImgAbove(y,x) = cImg(y,x) / y;
-    meanImgBelow(y,x) = (cImg(ny,x) - cImg(y,x)) / (ny-y+1);
-  end
+mask = zeros(ny,nx, type);
+for x = 1:nx
+  mask(:,x) = 1:ny;
 end
+
+meanImgAbove = cImg ./ mask;
+meanImgBelow = flipud( cImgFlipped ./ mask );
 
 end
